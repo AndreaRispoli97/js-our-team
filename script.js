81b39c1f386ed52449f684057577a7c2e45de304
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-10 16:51:46 +0100

    add: aggiunta parte js e fix nella parte css

--- script.js
+++ script.js
@@ -38,3 +38,33 @@ const teamMembers = [
 ];
 
 console.log('Andrea')
+
+//Andiamo a selezionare il container principale dove vogliamo inserire le card
+
+const teamContainer = document.querySelector('.container-main');
+
+//Creiamo una funzione con un ciclo forEach che andrà a prendere ogni dato dell'Array teamMembers
+
+teamMembers.forEach(function(member) {
+  //Andiamo a distrutturare l'oggetto per ogni membro che vogliamo estrarre 
+  const {name, role, email, img} = member;
+  //Creiamo la card e andiamo a incollare la card precedentemente creata nell'html
+  //Usiamo ${} per inserire le variabili all'interno della stringa
+  const card = `
+    <section class="team">
+            <div>
+            
+                <img src="${img}" alt="${name}" class="team-member-img">
+            </div>
+            <div class="team-member">
+                <h2 class="team-member-name">${name}</h2>
+                <p class="team-member-role">${role}</p>
+                <a href="#" class="team-member-email">${email}</a>
+
+            </div>
+
+        </section>
+  `;
+  //Andiamo ad inserire la card all'interno del container principale per poi stamparla nell'html
+  teamContainer.innerHTML += card;
+});
